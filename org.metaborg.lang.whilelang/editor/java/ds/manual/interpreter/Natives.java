41f6e8baf996ca01322e83e879069a0a35cabc0e
Vlad Vergu <v.vergu@gmail.com>
2014-09-10 17:07:51 +0300

    Implemented the required native functions

---
+++ org.metaborg.lang.whilelang/editor/java/ds/manual/interpreter/Natives.java
@@ -0,0 +1,21 @@
+package ds.manual.interpreter;
+
+public class Natives {
+
+	public static int int_times_2(int v1, int v2) {
+		return v1 * v2;
+	}
+
+	public static int int_plus_2(int v1, int v2) {
+		return v1 + v2;
+	}
+
+	public static int int_minus_2(int v1, int v2) {
+		return v1 - v2;
+	}
+
+	public static boolean int_gt_2(int v1, int v2) {
+		return v1 > v2;
+	}
+
+}
